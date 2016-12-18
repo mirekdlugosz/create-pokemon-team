99188304deda9f38985ca202e42e9b6b893fd1c6
Mirosław Zalewski <mz@miroslaw-zalewski.eu>
2016-12-18 22:30:44 +0100

    Refactor frontend code - selection working as expected

--- frontend/js/controller.js
+++ frontend/js/controller.js
@@ -1,45 +1,86 @@
 
 angular.module('pokemonTeamBuilder', [])
-	.factory('PokemonList', ['$http', function($http) {
-		var self = this;
-		self.pokemon = [];
+.factory('PokemonList', ['$http', function($http) {
+	var self = this;
+	self.pokemon = [];
 
-		self.getPokemon = function() {
-			var promise = $http.get('http://localhost:5000/pokemon').then(function(response) {
-				self.pokemon = response.data;
-				return self.pokemon;
-			}, function(errResponse) {
-				console.error('Error downloading Pokemon list');
-			});
-			return promise;
+	self.getPokemonList = function() {
+		var promise = $http.get('http://localhost:5000/pokemon').then(function(response) {
+			self.pokemon = response.data;
+			return self.pokemon;
+		}, function(errResponse) {
+			console.error('Error downloading Pokemon list');
+		});
+		return promise;
+	}
+
+	return {
+		getPokemonList: self.getPokemonList
+	}
+}])
+.service('PokemonMoves', ['$http', function($http) {
+	this.moves = [];
+
+	this.getMoves = function(pokemon_name) {
+		var promise = $http.get('http://localhost:5000/pokemon/' + pokemon_name + '/moves');
+		return promise;
+	};
+}])
+.factory('PokemonTeam', ['PokemonList', function(PokemonList) {
+	var self = this;
+	self.team = [
+		{
+			pokemon: "",
+			moves: ["", "", "", ""],
+		},
+		{
+			pokemon: "",
+			moves: ["", "", "", ""],
+		},
+		{
+			pokemon: "",
+			moves: ["", "", "", ""],
+		},
+		{
+			pokemon: "",
+			moves: ["", "", "", ""],
+		},
+		{
+			pokemon: "",
+			moves: ["", "", "", ""],
+		},
+		{
+			pokemon: "",
+			moves: ["", "", "", ""],
 		}
+	];
 
-		return {
-			getPokemon: self.getPokemon
-		}
-	}])
-	.service('PokemonMoves', ['$http', function($http) {
-		this.moves = [];
+	self.getTeam = function() {
+		return self.team;
+	}
 
-		this.getMoves = function(pokemon_name) {
-			var promise = $http.get('http://localhost:5000/pokemon/' + pokemon_name + '/moves');
-			return promise;
-		};
-	}])
-	.controller('PokemonListCtrl', ['PokemonList', function(pokemonList) {
+	return {
+		get: self.getTeam
+	}
+}])
+.controller('PokemonTeamController', 
+	['PokemonTeam', 'PokemonMoves', function(PokemonTeam, PokemonMoves) {
 		var self = this;
-		pokemonList.getPokemon().then(function(pokemon_list) {
-			self.pokemon = pokemon_list;
-		})
-	}])
-	.controller('PokemonMovesCtrl', ['PokemonMoves', function(PokemonMoves) {
-		var self = this;
-		self.moves = [];
+		self.members = PokemonTeam.get();
 
-		self.getMoves = function() {
-			PokemonMoves.getMoves(self.pokemon).then(function(pokemon_moves) {
-				self.moves = pokemon_moves.data;
+		self.getMoves = function(member_object) {
+			console.log(member_object);
+			var pokemon_name = member_object.pokemon.id;
+			PokemonMoves.getMoves(pokemon_name).then(function(pokemon_moves) {
+				member_object.available_moves = pokemon_moves.data;
+				member_object.moves = ["", "", "", ""];
 			})
 		}
+}])
+.controller('PokemonListController', ['PokemonList', function(PokemonList) {
+	var self = this;
 
-	}])
+	PokemonList.getPokemonList().then(function(pokemon_list) {
+		self.available_pokemon = pokemon_list;
+	})
+}])
