b7338388844626b93612ea354ae0b43e9f48f54d
Mirosław Zalewski <mz@miroslaw-zalewski.eu>
2017-01-24 00:35:28 +0100

    Add CORS header and move headers to global

--- backend/api/pokemon.py
+++ backend/api/pokemon.py
@@ -2,20 +2,24 @@ import os
 import flask
 
 DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+DEFAULT_HEADERS = {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    }
 
 app = flask.Flask(__name__)
 
 @app.errorhandler(404)
 def not_found(e):
     response_text = """{"error": "404 - no such resource"}"""
-    return flask.make_response(response_text, 404, {"Content-Type": "application/json"})
+    return flask.make_response(response_text, 404, DEFAULT_HEADERS)
 
 @app.route('/pokemon')
 def api_root():
     with open(os.path.join(DIRECTORY, "pokemon-list.json"), 'r') as fh:
         js = fh.read()
 
-    return flask.make_response(js, 200, {"Content-Type": "application/json"})
+    return flask.make_response(js, 200, DEFAULT_HEADERS)
 
 @app.route('/pokemon/<pokemon_name>/moves')
 def api_articles(pokemon_name):
@@ -26,7 +30,7 @@ def api_articles(pokemon_name):
     with open(move_file_path, 'r') as fh:
         js = fh.read()
 
-    return flask.make_response(js, 200, {"Content-Type": "application/json"})
+    return flask.make_response(js, 200, DEFAULT_HEADERS)
 
 if __name__ == '__main__':
     app.run()
