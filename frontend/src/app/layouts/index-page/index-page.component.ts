682cb5e6333997c43be5dc042767a6542186e688
Alex Hoffman <alexhoffman617@gmail.com>
2018-10-03 00:08:35 -0400

    fixing style

--- frontend/src/app/layouts/index-page/index-page.component.ts
+++ frontend/src/app/layouts/index-page/index-page.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { TitleService } from '../../services/title.service';
+
 @Component({
   selector: 'app-index-page',
   templateUrl: './index-page.component.html'
 })
 export class IndexPageComponent implements OnInit {
   title = 'createPokémon.​team';
+  
   constructor(private titleService: TitleService) {}
 
   ngOnInit() {
@@ -17,4 +19,5 @@ export class IndexPageComponent implements OnInit {
       }
     });
   }
+  
 }
